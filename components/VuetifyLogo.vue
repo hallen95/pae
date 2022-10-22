052d0d30607425a2c957d15bf3f528bcde73587e
BrianBitopia <brian@bitopia.digital>
2022-10-22 15:14:29 -0300

    first-commit

---
+++ components/VuetifyLogo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="">
+    <img
+      class="mew__image"
+      alt="Mew Image"
+      :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${src}.png`"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Image",
+  props: {
+    src: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+.mew__image {
+  height: 180px;
+  width: 180px;
+  transform: rotateY(560deg);
+  animation: turn 3.5s ease-out forwards 1s;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <VuetifyLogo :src="mew.id" />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          {{ mew.name }}
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Vuetify is a progressive Material Design component framework for
+            Vue.js. It was designed to empower developers to create amazing
+            applications.
+          </p>
+          <p>
+            If you have questions, please join the official
+            <a
+              href="https://chat.vuetifyjs.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="chat"
+            >
+              discord </a
+            >.
+          </p>
+          <p>
+            Find a bug? Report it on the github
+            <a
+              href="https://github.com/vuetifyjs/vuetify/issues"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="contribute"
+            >
+              issue board </a
+            >.
+          </p>
+          <p>
+            Thank you for developing with Vuetify and I look forward to bringing
+            more exciting features in the future.
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3" />
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt Documentation
+          </a>
+          <br />
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  async asyncData({ $axios }) {
+    const response = await $axios.get("/pokemon/mew");
+    const mew = {
+      data: response.data,
+      name: response.name,
+      stats: response.stats,
+      id: response.data.id,
+    };
+    return {
+      mew,
+    };
+  },
+  data() {
+    return {
+      data: null,
+    };
+  },
+};
+</script>
